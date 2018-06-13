a855c69303eecf87120065adb990ab14109bf4d5
Davide <davidde85@hotmail.it>
2018-06-13 15:38:41 +0200

    added hostnames in kylo-ui.yaml, other stuff for log and pods scripts

--- scripts/nifilogs.sh
+++ scripts/nifilogs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-nifiservicepod=$(kubectl get pods --selector=io.kompose.service=tba-nifi -o yaml | sed -En 's/name: tba-nifi-(.*)-(.*)/tba-nifi-\1-\2/p')
+nifiservicepod=$(kubectl get pods -o yaml | sed -En 's/name: tba-nifi-(.*)-(.*)/tba-nifi-\1-\2/p')
 echo "NiFi pod is $nifiservicepod"
 kubectl exec -it $nifiservicepod -- tail -1000f /usr/nifi/logs/nifi-app.log
