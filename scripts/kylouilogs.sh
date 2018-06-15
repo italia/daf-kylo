bb5d0f79f4ccbe4722eb03a6c53aceb27e6893d4
Davide <davidde85@hotmail.it>
2018-06-15 08:36:42 +0200

    Updated logs and pods scripts

--- scripts/kylouilogs.sh
+++ scripts/kylouilogs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-kylouipod=$(kubectl get pods --selector=io.kompose.service=tba-kylo-ui -o yaml | sed -En 's/name: tba-kylo-ui-(.*)-(.*)/tba-kylo-ui-\1-\2/p')
+kylouipod=$(kubectl get pods -o yaml | sed -En 's/name: tba-kylo-ui-(.*)-(.*)/tba-kylo-ui-\1-\2/p')
 echo "kylo ui pod is $kylouipod"
 kubectl exec -it $kylouipod -- tail -f /var/log/kylo-ui/kylo-ui.log
