6a69267656d6797f6295ca68929e9be4be8182a6
Davide <davidde85@hotmail.it>
2018-06-19 16:47:59 +0200

    Edited port-forward.sh script

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -5,7 +5,7 @@ case $1 in
 kylo-ui)
     kylouipod=$(kubectl get pods -o yaml | sed -En 's/name: tba-kylo-ui-(.*)-(.*)/tba-kylo-ui-\1-\2/p')
     echo "kylo ui pod is kylouipod"
-    kubectl exec -it $kyloservicepod -- tail -1000f /var/log/kylo-services/kylo-services.log
+    kubectl port-forward $kylouipod 8400:8400
   ;;
 nifi)
     nifipod=$(kubectl get pods -o yaml | sed -En 's/name: tba-nifi-(.*)-(.*)/tba-nifi-\1-\2/p')
