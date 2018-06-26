f1e28ca4061629f718897a58c09844a9010d8c51
Davide <davidde85@hotmail.it>
2018-06-26 14:42:50 +0200

    Deleted kylo-services-spark in deployment, in playbook.sh and in cleanup.sh. Edited port-forward to use production and test environment.

--- scripts/port-forward.sh
+++ scripts/port-forward.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+if [ "$1" = 'test' ]
+then
+    export KUBECONFIG='/home/davide/.kube_test.conf'
+    echo $KUBECONFIG
+elif [ "$1" = 'prod' ]
+    then
+    export KUBECONFIG='/home/davide/.kube.conf'
+    echo $KUBECONFIG
+fi
 
-case $1 in
+echo $ENV
+
+case $2 in
 
 kylo-ui)
     kylouipod=$(kubectl get pods -o yaml | sed -En 's/name: tba-kylo-ui-(.*)-(.*)/tba-kylo-ui-\1-\2/p')
@@ -22,5 +33,8 @@ mysql)
     echo "mysql service pod is $mysqlpod"
     kubectl port-forward $mysqlpod 3306:3306
     ;;
+*)
+    echo "Sorry, I can not get a $2 pod for you!"
+    echo "Cases are nifi, kylo-ui, mysql, activemq";;
 esac
 
