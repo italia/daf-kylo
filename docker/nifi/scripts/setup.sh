a030c77cd40f8048ee0646ff79d44972cbea9b99
davidegazze <davide.gazze@gmail.com>
2018-07-19 19:05:09 +0200

    Modification to NIFI pod for enabling cluster

---
+++ docker/nifi/scripts/setup.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+set -x
+
+# Two steps needed to avoid "Device or resource busy" error inside Docker
+cat /etc/resolv.conf | sed "s/^search \(.*\)/search ${NIFI_ADDITIONAL_SEARCH_DOMAINS:-} \1/g" > /tmp/resolv.conf
+cp /tmp/resolv.conf /etc/resolv.conf
+
+
+# Update nifi properties
+
+mkdir -p /usr/nifi/conf
+cp --preserve=links /usr/nifi/conf.temp/* /usr/nifi/conf
+
+nifi_props_file="/usr/nifi/conf/nifi.properties"
+prop_replace () {
+  target_file=${3:-${nifi_props_file}}
+  echo 'replacing target file ' ${target_file}
+  sed -i -e "s|^$1=.*$|$1=$2|"  ${target_file}
+}
+
+HOSTNAME=$(hostname)
+prop_replace 'nifi.web.http.host'               "$HOSTNAME"
+prop_replace 'nifi.remote.input.host'           "$HOSTNAME"
+prop_replace 'nifi.cluster.node.address'        "$HOSTNAME"
