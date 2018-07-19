90170aaa7c2418621efc9adfcc805dc31ae7052f
davidegazze <davide.gazze@gmail.com>
2018-07-19 19:08:53 +0200

    Release Candidate for Nifi cluster service

---
+++ scripts/nifipod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+nifiservicepod="tba-nifi-0"
+
+case $1 in
+0)
+    nifiservicepod="tba-nifi-0"
+;;
+1)
+    nifiservicepod="tba-nifi-1"
+;;
+
+*)
+    echo "Sorry, you specifiy a not existed pod!"
+    echo "Use just 0 or 1";;
+esac
+
+echo "NiFi pod is $nifiservicepod"
+kubectl exec -it $nifiservicepod -- /bin/bash
