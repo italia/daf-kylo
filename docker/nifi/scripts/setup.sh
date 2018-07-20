ee90955e8d6cf9ec4a02b4637fccf7b47a2b3aae
davidegazze <davide.gazze@gmail.com>
2018-07-20 17:15:18 +0200

    RC2 of NIFI cluster

--- docker/nifi/scripts/setup.sh
+++ docker/nifi/scripts/setup.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 set -x
 
+# Remove duplicates
+cat /etc/resolv.conf | sed 's/tba-nifi.default.svc.cluster.local teamdigitale.test/\1/g' > /tmp/resolv.conf
+cp /tmp/resolv.conf /etc/resolv.conf
 # Two steps needed to avoid "Device or resource busy" error inside Docker
 cat /etc/resolv.conf | sed "s/^search \(.*\)/search ${NIFI_ADDITIONAL_SEARCH_DOMAINS:-} \1/g" > /tmp/resolv.conf
 cp /tmp/resolv.conf /etc/resolv.conf
@@ -12,7 +15,7 @@ mkdir -p /usr/nifi/conf
 cp --preserve=links /usr/nifi/conf.temp/* /usr/nifi/conf
 
 nifi_props_file="/usr/nifi/conf/nifi.properties"
-prop_replace () {
+    prop_replace () {
   target_file=${3:-${nifi_props_file}}
   echo 'replacing target file ' ${target_file}
   sed -i -e "s|^$1=.*$|$1=$2|"  ${target_file}
