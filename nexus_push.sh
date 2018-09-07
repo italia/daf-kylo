fcc6cd53afa2e1b83484f3d28f11c4b01220f6fc
davidegazze <davide.gazze@gmail.com>
2018-09-07 12:31:38 +0200

    Update list of active images

--- nexus_push.sh
+++ nexus_push.sh
@@ -10,14 +10,10 @@ case $1 in
     docker push nexus.teamdigitale.test/tba-nifi.1.6.0:1.2.6-CLUSTER
 	;;
   prod)
-    docker push nexus.default.svc.cluster.local:5000/tba-mysql.10.3:1.1.0
-    docker push nexus.default.svc.cluster.local:5000/tba-activemq.5.15.1:1.1.0
-    docker push nexus.default.svc.cluster.local:5000/tba-kylo-services.8.4:1.1.0
-    docker push nexus.default.svc.cluster.local:5000/tba-kylo-ui.8.4:1.1.0
-    docker push nexus.default.svc.cluster.local:5000/tba-nifi.1.4.0:1.1.1-SNAPSHOT
+    docker push nexus.daf.teamdigitale.it/tba-mysql.10.3:1.1.0
+    docker push nexus.daf.teamdigitale.it/tba-activemq.5.15.1:1.1.0
+    docker push nexus.daf.teamdigitale.it/tba-kylo-services.9.1:1.0.1
+    docker push nexus.daf.teamdigitale.it/tba-kylo-ui.9.1:1.0.0
+    docker push nexus.daf.teamdigitale.it/tba-nifi.1.6.0:1.0.1-CLUSTER
 	;;
 esac
-
-    docker push 192.168.20.52/tba-kylo-services.9.1:1.0.5-FIX
-    docker push 192.168.20.52/tba-kylo-ui.9.1:1.0.5-FIX
-    docker push 192.168.20.52/tba-nifi.1.6.0:1.2.5-CLUSTER
